c1006343f9a82af5ada25df1c9d7ac9a6f37b8a3
hezhensong <hezhensong@bytedance.com>
2017-12-16 23:42:38 +0800

    009

---
+++ Math/009.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+
+class Solution(object):
+    """
+    Palindrome Number
+    """
+
+    @staticmethod
+    def is_palindrome(x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        s = str(x)
+
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+
+        return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    a = 232
+    print solution.is_palindrome(a)
+
+    a = 245
+    print solution.is_palindrome(a)
